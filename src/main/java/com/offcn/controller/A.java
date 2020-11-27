ba00a03b3269b8f31df73a56cf75e12d162a7898
zhaocan <zhiyuketang@163.com>
2020-11-27 10:39:12 +0800

    测试提交代码

---
+++ src/main/java/com/offcn/controller/A.java
@@ -0,0 +1,8 @@
+package com.offcn.controller;
+
+/**
+ * 作者：赵灿
+ * 日期：2020-11-27
+ */
+public class A {
+}
--- src/main/java/com/offcn/controller/TestController.java
+++ src/main/java/com/offcn/controller/TestController.java
@@ -4,8 +4,11 @@ package com.offcn.controller;
  * 作者：赵灿
  * 日期：2020-11-27
  */
+// 分享本地项目到GitHub上：  VCS -> Import into Version Control -> Share Project on GitHub
 public class TestController {
     public void hello(){
+        // 提交代码到本地仓库：在修改的位置 右键 -> Git -> Commit Directory
+        // 提交本地修改后的代码到GitHub：
         System.out.println("本地修改，提交到Github上");
     }
 }
